d0dd23c86d5df6d99c01c233d89c11d6b1ad92e2
ucolinmee <colinjmeehan23@gmail.com>
2024-06-26 10:46:06 +0800

    Integrated both APIs together

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
 const GIF_API_KEY = 'jhnZ4FYM2Th5GS1EP87xTch1IxR7lcIb';
+const WEATHER_API_KEY = 'ed685752c9664abeae8122501242406';
 
-
-const searchBtn = document.getElementById('search-btn');
-const img = document.querySelector('img');
-
-searchBtn.addEventListener('click', () => {
-    const searchText = document.querySelector('input').value;
-    const URL = `http://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${searchText}`;
-
-    retrieveGIF(URL);
+const submitBtn = document.querySelector('button');
+submitBtn.addEventListener('click', () => {
+    const city = document.getElementById('city').value;
+    const weather_request = `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${city}`;
+    retrieveWeatherData(weather_request).then((result) => {
+        const gif_request = `http://api.giphy.com/v1/gifs/translate?api_key=${GIF_API_KEY}&s=${result.city} ${result.description}`;
+        retrieveGIF(gif_request).then((result) => {
+            const img = document.querySelector('img');
+            img.src = result;
+        })
+    });
 })
 
-async function retrieveGIF(url) {
+async function retrieveGIF(request) {
     try {
-        const response = await fetch(url, {mode: 'cors'});
+        const response = await fetch(request, {mode: 'cors'});
         const gifData = await response.json();
-        img.src = gifData.data.images.original.url;
+        return gifData.data.images.original.url;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function retrieveWeatherData(request) {
+    try {
+        const response = await fetch(request, {mode: 'cors'});
+        const weatherData = await response.json();
+        return {
+            city: weatherData.location.name,
+            description: weatherData.current.condition.text,
+            temperature: weatherData.current.temp_c
+        }
     } catch (err) {
         console.log(err);
     }
